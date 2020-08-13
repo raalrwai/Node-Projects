c8ca46a356f55bbdff39f0728b0c6a30ede375cb
Rami Alrwais <raalrwai@ucsc.edu>
2020-08-13 14:13:17 -0700

    Added isLoading to authentication

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import CreateDiet from "./components/create-diet.component";
 import Navbar from "./components/navbar.component";
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
-
+import { useAuth0 } from '@auth0/auth0-react';
  import Profile from './components/Profile'; 
 
 
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
 
   return (
     <>
